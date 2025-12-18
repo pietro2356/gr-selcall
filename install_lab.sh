270f9de3232d64d219dc6ade91f8ca8b3c1e11f4
Pietro Rocchio <pietro.rocchio@gmail.com>
2025-12-18 16:52:24 +0100

    build: 🛠️ update install script to correctly manage PYTHONPATH and provide user instructions

--- install_lab.sh
+++ install_lab.sh
@@ -1,4 +1,4 @@
-#! /bin/bash
+#!/bin/bash
 
 # Remove any existing build directory
 rm -rf ./build
@@ -10,12 +10,15 @@ cmake -DCMAKE_INSTALL_PREFIX=/home/ngn/.grc_gnuradio/ ..
 make
 make install
 
+grc_local_path="/home/ngn/.grc_gnuradio/lib/python3.10/site-packages/"
+
 # Ensure the local Python path is included in PYTHONPATH
-if [[ ":$PYTHONPATH:" == *":/home/ngn/.grc_gnuradio/lib/python3.10/site-packages/:"* ]]; then
+if [[ ":$PYTHONPATH:" == *":$grc_local_path:"* ]]; then
   echo "Local path is correctly set in 'PYTHONPATH'"
 else
   echo "Local path is missing in 'PYTHONPATH', adding it now."
-  echo "export PYTHONPATH=$PYTHONPATH:/home/ngn/.grc_gnuradio/lib/python3.10/site-packages/" >> ~/.bashrc
+  echo "export PYTHONPATH=\$PYTHONPATH:$grc_local_path" >> ~/.bashrc
   source ~/.bashrc
   echo "Local path added to 'PYTHONPATH' via ~/.bashrc"
+  echo "Please restart your terminal or run 'source ~/.bashrc' to apply the changes."
 fi
